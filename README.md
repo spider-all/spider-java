d7eb3d8f5d64aa405378fb7eec91f93e15607c53
Tosone <i@tosone.cn>
2020-03-31 16:55:41 +0800

    :tada: First commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# spider-java
+
+Crawl GitHub APIs and store the discovered orgs, repos, commits...
+
+## Run in terminal
+
+``` bash
+gradle jar && java -jar build/libs/spider.jar
+```
+
+### Run in docker
+
+``` bash
+```
---
+++ src/main/java/spider/App.java
@@ -0,0 +1,33 @@
+package spider;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+  public String getGreeting() {
+    return "Hello world.";
+  }
+
+  public static void main(String[] args) {
+    Connection conn = null;
+    try {
+      String url = "jdbc:sqlite:spider.db";
+      conn = DriverManager.getConnection(url);
+
+      System.out.println("Connection to SQLite has been established.");
+
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    } finally {
+      try {
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (SQLException ex) {
+        System.out.println(ex.getMessage());
+      }
+    }
+    System.out.println(new App().getGreeting());
+  }
+}
